2319f425487d2ded3efc48c2e63d821333f82add
kaito hasegawa <kaitohasegawa000@gmail.com>
2019-10-02 13:03:44 +0900

    テスト完了

--- db/schema.rb
+++ db/schema.rb
@@ -29,12 +29,14 @@ ActiveRecord::Schema.define(version: 20190929114055) do
   end
 
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.text     "body",       limit: 65535
+    t.text     "body",       limit: 65535, null: false
     t.string   "image"
     t.integer  "group_id",                 null: false
     t.integer  "user_id",                  null: false
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.index ["group_id"], name: "index_messages_on_group_id", using: :btree
+    t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -53,4 +55,6 @@ ActiveRecord::Schema.define(version: 20190929114055) do
 
   add_foreign_key "group_users", "groups"
   add_foreign_key "group_users", "users"
+  add_foreign_key "messages", "groups"
+  add_foreign_key "messages", "users"
 end
