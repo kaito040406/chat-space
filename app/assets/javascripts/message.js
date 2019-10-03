8e6ac16194a5de48fdd089573755af74976edaf3
kaito hasegawa <kaitohasegawa000@gmail.com>
2019-10-03 16:42:27 +0900

    非同期通信において画像の投稿の実装
    非同期通信において画像の投稿の実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,13 +1,12 @@
 $(function(){
-  function buildMessage(message){
-    var html = `<div class="message-box">
+  function buildMessage1(message){
+    var html1 = `<div class="message-box">
                   <div class="message__upper-info">
                     <p class="message__upper-info__talker">
                       ${message.name}
                     </p>
                     <p class="message__upper-info__date">
                       ${message.date}
-                      
                     </p>
                   </div>
                     <p class="message__text">
@@ -16,8 +15,31 @@ $(function(){
                     </p>
                     <p></p>
                 </div>`
-    return html;
+    return html1;
   }
+
+  function buildMessage2(message){
+    var html2 = `<div class="message-box">
+                  <div class="message__upper-info">
+                    <p class="message__upper-info__talker">
+                      ${message.name}
+                    </p>
+                    <p class="message__upper-info__date">
+                      ${message.date}
+                    </p>
+                  </div>
+                    <p class="message__text">
+                    </p><p class="lower-message__content">
+                    ${message.body}
+                    </p>
+                    <img class="lower-message__image" src="${message.image.url}" alt="Ph thumb"></img>
+                    <p></p>
+                </div>`
+    return html2;
+  }
+
+
+
 
   $('#new_message').on('submit', function(e){
     e.preventDefault();
@@ -32,11 +54,17 @@ $(function(){
       contentType: false
     })
     .done(function(message){
-      var html = buildMessage(message); 
-      $('.messages').append(html);
-      $('#message_body').val('');
-      console.log("投稿OK");
-
+      if (message.image.url == null){
+        var html1 = buildMessage1(message); 
+        $('.messages').append(html1);
+        $('#message_body').val('');
+        console.log("画像なし投稿OK");
+      } else{
+        var html2 = buildMessage2(message); 
+        $('.messages').append(html2);
+        $('#message_body').val('');
+        console.log("画像あり投稿OK");
+      }
       $('html, body').animate({
         scrollTop: $(document).height()
       },2000);
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,3 +1,4 @@
 json.name @message.user.name
 json.body @message.body
-json.date @message.created_at
+json.date @message.created_at.strftime("%Y/%m/%d %H:%M")
+json.image @message.image
