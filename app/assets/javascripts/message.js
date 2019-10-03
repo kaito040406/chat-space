1a6db53dfe93985411e07a14bd2f28c1f6c9273a
kaito hasegawa <kaitohasegawa000@gmail.com>
2019-10-03 20:12:19 +0900

    改行の削除
    無駄な改行があったため削除を行った。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -18,11 +18,6 @@ $(function(){
                 </div>`
     return html;
   }
-
-
-
-
-
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
