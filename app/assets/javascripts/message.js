13d44144d263a74768bc652e7b404a57c8b49b44
kaito hasegawa <kaitohasegawa000@gmail.com>
2019-10-03 12:32:24 +0900

    イベント発火２
    sendボタンを押し
    押したらコンソール(OK)を出し、
    正しく機能していることを確認した

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,7 +3,23 @@ $(function(){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action');
-    console.log("OK");
-    
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      console.log("OK")
+    })
+    .fail(function(){
+      alert('error');
+    })
+    .always(() => {
+      $(".submit-btn").removeAttr("disabled");
+     
+    });
   });
 });
