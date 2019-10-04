59c4a7a15cf1c4f9b9f6830d7422bfe545f4015e
kaito hasegawa <kaitohasegawa000@gmail.com>
2019-10-04 17:33:36 +0900

    検索機能の追加
    インクリメンタルサーチによる検索機能を追加

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,13 +1,28 @@
 $(function() {
 
   function appendUser(user){
-    console.log("OK1")
-    var html = `<div class="chat-group-user clearfix">
-                  <p class="chat-group-user__name">${ user.name }</p>
-                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="60" data-user-name="ll">追加</a>
-                </div>`
+    
+    var html = `
+                <div class = "search-box">
+                  <div class="chat-group-user clearfix">
+                    <p class="chat-group-user__name">${ user.name }</p>
+                    <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="60" data-user-name="ll">追加</a>
+                  </div>
+                </div>
+                `
     return html;
     }
+
+  function appendMsg(){
+    var html = `
+                <div class = "search-box">
+                  <div class="chat-group-user clearfix">
+                    <p class="chat-group-user__name">人物はいません</p>
+                  </div>
+                </div>
+               `
+    return html;
+  }
 
   $("#user-search-field").on("keyup", function(){
     var input = $("#user-search-field").val();
@@ -16,18 +31,22 @@ $(function() {
       url:  '/users',
       data: { keyword: input },
       dataType: 'json'
-    })
-   
+    })  
     .done(function(users){
+      console.log("OK1");
+      $(".search-box").empty();
       if(users.length !== 0) {
         users.forEach(function(user){
           $('#user-search-result').append(appendUser(user));
-          console.log("OK2");
         })
       }
       else{
-        appendUser('そんな人間はござらん')
+        console.log("OK1");
+        $('#user-search-result').append(appendMsg());
       }
     })
-  })
-})
+    .fail(function(){
+      alert('検索に失敗しました');
+    })
+  });
+});
