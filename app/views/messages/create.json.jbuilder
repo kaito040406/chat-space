a627c809152aa6bcb38f773faf34440d53f4df97
kaito hasegawa <kaitohasegawa000@gmail.com>
2019-10-07 13:16:50 +0900

    ルーティン追加

--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -2,3 +2,6 @@ json.name @message.user.name
 json.body @message.body
 json.date @message.created_at.strftime("%Y/%m/%d %H:%M")
 json.image @message.image
+
+json.(@message, :content, :imate)
+json.id @message.id
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   root 'groups#index'
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update, :index, :search] do
-
     resources :messages, only: [:index, :create]
+
+    namespace :api do
+      resource :messages, only: :index, defaults: { format: 'json' }
   end
-  resources :users, only: [:index, :edit, :update]
 end
