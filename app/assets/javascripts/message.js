012299818027312886b1782e1343bafd02f86122
kaito hasegawa <kaitohasegawa000@gmail.com>
2019-10-07 23:23:41 +0900

    途中経過

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -49,7 +49,7 @@ $(function(){
   });
 
     var buildMessageHTML = function(message) {
-      if (message.content && message.image.url) {
+      if (message.text && message.image.url) {
         //data-idが反映されるようにしている
         var html = '<div class="message" data-id=' + message.id + '>' +
           '<div class="upper-message">' +
@@ -62,12 +62,13 @@ $(function(){
           '</div>' +
           '<div class="lower-message">' +
             '<p class="lower-message__content">' +
-              message.content +
+              message.body +
             '</p>' +
             '<img src="' + message.image.url + '" class="lower-message__image" >' +
           '</div>' +
         '</div>'
-        }else if (message.content) {
+        }else if (message.text) {
+          console.log("OK");
           //同様に、data-idが反映されるようにしている
           var html = '<div class="message" data-id=' + message.id + '>' +
             '<div class="upper-message">' +
@@ -80,7 +81,7 @@ $(function(){
             '</div>' +
             '<div class="lower-message">' +
               '<p class="lower-message__content">' +
-                message.content +
+                message.body +
               '</p>' +
             '</div>' +
           '</div>'
@@ -106,16 +107,28 @@ $(function(){
 
   var reloadMessages = function(){
     last_message_id = $('.message-box').last().attr('id');
-    console.log(last_message_id);
-    var urldata = "groups/" + last_message_id;
+    group_id = $('.main-header__left-box__current-group').attr('id')
+    i = 0;
     $.ajax({
-      url: urldata,
-      type: 'GET',
+      url: '/groups/' + group_id + '/api/messages',
+      type: 'get',
       dagaType: 'json',
       data: {id: last_message_id}
     })
     .done(function(messages){
-      console.lod('success');
+      insertHTML = null;
+      messages.forEach(function(message){
+        var addhtml = buildMessageHTML(message);       
+        insertHTML = insertHTML + addhtml; 
+        i = i + 1;
+      })
+      if(last_message_id < messages.id){
+        $('.messages').append(insertHTML);
+        i = 0;
+      };
+      $('html, body').animate({
+        scrollTop: $(document).height()
+      },2000);
     })
     .fail(function(){
       console.log('error');
